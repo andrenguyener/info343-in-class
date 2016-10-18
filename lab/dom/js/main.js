ee7c99e1d15848fe6cc04a2825f5b225d1a216da
Andre <andre97@uw.edu>
2016-10-18 15:21:16 -0700

    transition exercise

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,14 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.addEventListener("click", fadeToBlack);
+
+function fadeToBlack() {
+	boxOne.classList.add("fade-to-black");
+	boxOne.classList.remove();
+}
+
+
+
+	
