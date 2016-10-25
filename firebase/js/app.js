a15bc68f7a808a596344c6ddeb9ed5237832ee31
Andre <andre97@uw.edu>
2016-10-25 12:18:03 -0700

    Firebase demo inclass

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -12,3 +12,108 @@
 //  to the URL provided by Firebase 
 
 //paste the Firebase initialization code here
+
+// Initialize Firebase
+var config = {
+    // apiKey: "AIzaSyB-nnbJgC5sdmORm1-fivLJfP3Mzw-A9e4",
+    // authDomain: "tasks-demo-4886c.firebaseapp.com",
+    // databaseURL: "https://tasks-demo-4886c.firebaseio.com",
+    // storageBucket: "tasks-demo-4886c.appspot.com",
+    // messagingSenderId: "502775213634"
+    apiKey: "AIzaSyAevP7I3aWCDnXzVexrwor3yuleEWqjq0g",
+    authDomain: "tasks-demo-be67b.firebaseapp.com",
+    databaseURL: "https://tasks-demo-be67b.firebaseio.com",
+    storageBucket: "tasks-demo-be67b.appspot.com",
+    messagingSenderId: "941361730310"
+
+};
+firebase.initializeApp(config);
+
+var currentUser;
+var authProvider = new firebase.auth.GithubAuthProvider();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser = user;
+        console.log(currentUser);
+    } else {
+        firebase.auth().signInWithRedirect(authProvider);
+    }
+});
+
+var taskForm = document.querySelector(".new-task-form");
+var taskTitleInput = taskForm.querySelector(".new-task-title");
+var taskList = document.querySelector(".task-list");
+var purgeButton = document.querySelector(".btn-purge");
+
+var tasksRef = firebase.database().ref("tasks");
+
+taskForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    var task = {
+        title: taskTitleInput.value.trim(),
+        done: false,
+        createdOn: firebase.database.ServerValue.TIMESTAMP,
+        createdBy: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email
+        }
+    };
+    tasksRef.push(task);
+
+    taskTitleInput.value = "";
+
+    return false;
+});
+
+function renderTask(snapshot) {
+    var task = snapshot.val();
+    var li = document.createElement("li");
+
+    //li.textContent = task.title;
+    var spanTitle = document.createElement("span");
+    spanTitle.textContent = task.title;
+    spanTitle.classList.add("task-title");
+    li.appendChild(spanTitle);
+
+    var spanCreation = document.createElement("span");
+    spanCreation.textContent = moment(task.createdOn).fromNow()
+    + " by "  + (task.createdBy.displayName || task.createdBy.email);
+    spanCreation.classList.add("task-creation");
+    li.appendChild(spanCreation);
+
+    if (task.done) {
+        li.classList.add("done");
+        purgeButton.classList.remove("hidden");
+    }
+
+    li.addEventListener("click", function() {
+       // console.log("click for " + task.title);
+       snapshot.ref.update({
+           done: !task.done
+       });
+    });
+
+    taskList.appendChild(li);
+}
+
+function render(snapshot) {
+    taskList.innerHTML = "";
+    purgeButton.classList.add("hidden");
+    snapshot.forEach(renderTask);
+}
+
+tasksRef.on("value", render);
+
+purgeButton.addEventListener("click", function() {
+    console.log("purge button clicked");
+    tasksRef.once("value", function(snapshot) {
+        snapshot.forEach(function(taskSnapshot) {
+            if (taskSnapshot.val().done) {
+                taskSnapshot.ref.remove();
+            }
+        });
+    });
+});
