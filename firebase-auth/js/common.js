1c919c18429c876d2d2b7a952534a23a00a9981e
Andre <andre97@uw.edu>
2016-10-27 12:19:53 -0700

    Review and firebase auth exsercise

--- firebase-auth/js/common.js
+++ firebase-auth/js/common.js
@@ -1,1 +1,10 @@
 //put your Firebase config and initialization code here
+var config = {
+    apiKey: "AIzaSyB-nnbJgC5sdmORm1-fivLJfP3Mzw-A9e4",
+    authDomain: "tasks-demo-4886c.firebaseapp.com",
+    databaseURL: "https://tasks-demo-4886c.firebaseio.com",
+    storageBucket: "tasks-demo-4886c.appspot.com",
+    messagingSenderId: "502775213634"
+};
+firebase.initializeApp(config);
+
